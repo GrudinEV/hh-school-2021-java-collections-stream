563e40ac4ce014d77564855a75c65d9d80b0def5
Evgeniy <grudin.ev@prg-it.ru>
2021-12-13 12:27:30 +0500

    Tasks complete

--- src/tasks/Task1.java
+++ src/tasks/Task1.java
@@ -5,6 +5,7 @@ import common.PersonService;
 import common.Task;
 
 import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 import java.util.Set;
 import java.util.stream.Collectors;
@@ -18,20 +19,21 @@ import java.util.stream.Collectors;
  */
 public class Task1 implements Task {
 
-  // !!! Редактируйте этот метод !!!
-  private List<Person> findOrderedPersons(List<Integer> personIds) {
-    Set<Person> persons = PersonService.findPersons(personIds);
-    return Collections.emptyList();
-  }
+    // !!! Редактируйте этот метод !!!
+    private List<Person> findOrderedPersons(List<Integer> personIds) {
+        Set<Person> persons = PersonService.findPersons(personIds);
+        // Ассимптотическая сложность алгоритма O(nlogn)
+        return persons.stream().sorted(Comparator.comparing(Person::getId)).collect(Collectors.toList());
+    }
 
-  @Override
-  public boolean check() {
-    List<Integer> ids = List.of(1, 2, 3);
+    @Override
+    public boolean check() {
+        List<Integer> ids = List.of(1, 2, 3);
 
-    return findOrderedPersons(ids).stream()
-        .map(Person::getId)
-        .collect(Collectors.toList())
-        .equals(ids);
-  }
+        return findOrderedPersons(ids).stream()
+                .map(Person::getId)
+                .collect(Collectors.toList())
+                .equals(ids);
+    }
 
 }
